22413c255418f83601f2abb3bb44358f93568f9d
Alejandrito97 <alex.hreus@gmail.com>
2022-12-20 15:21:20 -0800

    script opens text, reads text and counts words in the text

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+path_to_book = "books/frankenstein.txt"
 
 def main():
 
-    print("Hello world")
+    def read_book(path):
+        with open(path) as file:
+            book_content = file.read()
+            return book_content
+
+    
+    def word_count(text_to_count):
+        list_of_words = text_to_count.split()
+        return len(list_of_words)
+
+    text = read_book(path_to_book)
+    
+    num_of_words = word_count(text)
+    
+    print(num_of_words)
+    
 
 main()
